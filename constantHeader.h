58933e4be68876b0d526996ba48bc444139f9431
ensea <ensea@StudentLab>
2023-11-27 09:21:09 +0100

    Question 1

---
+++ constantHeader.h
@@ -0,0 +1,7 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+#define WELCOME_MESSAGE "$ ./enseash \nWelcome to ENSEA Tiny Shell. \nPour quitter, tapez 'exit'. \nenseash %\n"
