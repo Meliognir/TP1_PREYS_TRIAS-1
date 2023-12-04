16dcd82837b00a89613bb7dd84f804f599dfefed
ensea <ensea@StudentLab>
2023-12-04 11:05:29 +0100

    Global LAB

--- constantHeader.h
+++ constantHeader.h
@@ -8,13 +8,23 @@
 #include <fcntl.h>
 #include <string.h>
 
-//Constant for Q1 & Q2
+//Constant for Q1 
 #define WELCOME_MESSAGE "$ ./enseash \nWelcome to ENSEA Tiny Shell. \nPour quitter, tapez 'exit'. \nenseash %\n"
 
 //Constant for Q2
 #define BUFFERSIZE 128
 
 //Constant Q3
-#define EXIT_MESSAGE "enseash % exit\nBye bye...\n$\n"
+#define EXIT_MESSAGE "Bye bye...\n$\n"
+
+//Constant Q4
+#define ERROR0_MESSAGE "$ enseash [error : 0| %f ms]%" //when there are no errors
+#define ERROR1_MESSAGE "$ enseash [error : 1]%" // when there are errors
+
+//Constant Q5
+#define ERROR0_MESSAGE_SIZE 50 // Taille de la chaîne ERROR0_MESSAGE
+
+
+
 
 
